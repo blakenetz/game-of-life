c90c569ce717a99a1a06420de7444f25373036fa
Blake Netzeband <blake.netzeband@gmail.com>
2025-01-03 12:19:24 -0700

    update file structure by adding api file

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
 import { World, Results, Payload } from "@/types";
-
-const baseUrl = "https://game-of-life-service-ai3nmiz7aa-uc.a.run.app/";
-
-async function getWorld(): Promise<Payload> {
-  const url = new URL("world", baseUrl).toString();
-
-  const response = await fetch(url);
-  const world = await response.json();
-
-  return world;
-}
-
-function execute(world: World, count: number): World[] {
-  for (let i = 0; i < count; i++) {}
-  return [[[]]];
-}
+import { getWorld, postResults } from "./api";
 
 function evaluate({ id, generationCount, size, world }: Payload): Results {
   return {
@@ -24,17 +9,9 @@ function evaluate({ id, generationCount, size, world }: Payload): Results {
   };
 }
 
-async function postResults(results: Results) {
-  const url = new URL("results", baseUrl).toString();
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(results),
-  });
-
-  return response.json();
+function execute(world: World, count: number): World[] {
+  for (let i = 0; i < count; i++) {}
+  return [[[]]];
 }
 
 async function main() {
